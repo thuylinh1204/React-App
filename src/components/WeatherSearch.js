840ff3a611dc17bf56e66bc0aee64896e65f7478
thuylinh1204 <thuylinh1204@gmail.com>
2022-06-04 16:33:17 +0000

    Search by Location

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -6,7 +6,7 @@ const WeatherSearch = () => {
   return (
     <div className="weather-search">
   <form onSubmit={api_call} className="weather-search__form">
-    <input autoComplete="off" className="weather-search__input" type="text"/>
+    <input name = "location" autoComplete="off" className="weather-search__input" type="text"/>
     <div className="weather-search__submit">
       <button className="weather-search__button">&rarr;</button>
     </div>
