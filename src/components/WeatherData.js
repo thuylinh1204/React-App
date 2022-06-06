4b58586894bc9019ef3370176e87304158dc15f2
mminzwoo4155 <minzwoo1234@gmail.com>
2022-06-06 16:23:34 +0000

    Update calculation unit for WeatherData component

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -3,22 +3,22 @@ import React,  {useContext} from "react"
 import Context from "../Context"
 const WeatherData = () => {
   const {weather, city} = useContext(Context)
-  const {temp,humidity,pressure} = weather
+  const {temp, humidity, pressure} = weather
   return (
     <div className="weather-data">
   <p className="weather__tagline">Weather Information for <span className="weather-data__city">{city}</span></p>
   <div className="weather-data__box">
     <span className="weather-data__property">
       <p className="weather-data__title">Temperature</p>
-      <p className="weather-data__value">{temp}</p>
+      <p className="weather-data__value">{temp} ℃</p>
     </span>
     <span className="weather-data__property">
       <p className="weather-data__title">Humidity</p>
-      <p className="weather-data__value">{humidity}</p>
+      <p className="weather-data__value">{humidity} %</p>
     </span>
     <span className="weather-data__property">
       <p className="weather-data__title">Pressure</p>
-      <p className="weather-data__value">{pressure}</p>
+      <p className="weather-data__value">{pressure/1000} pa</p>
     </span>
   </div>
 </div>
